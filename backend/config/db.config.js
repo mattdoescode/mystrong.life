73039c3e30c906f3e8e82ee3f573db8407f86064
Matthew Loewen <matthewlanimates@gmail.com>
2021-08-03 17:40:45 -0400

    removed sequelize
    need to reimplement JWT + current routes

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -2,12 +2,5 @@ module.exports = {
     HOST: "localhost",
     USER: "root",
     PASSWORD: "password",
-    DB: "mystong-life",
-    dialect: "mysql",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
+    DB: "mystong-life"
   };
--- backend/server.js
+++ backend/server.js
@@ -1,74 +1,40 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const mysql = require('mysql');
 const cors = require("cors");
+const bodyParser = require("body-parser");
+
+//const port = process.env.PORT;
 
 const app = express();
 
+const config = require("./config/db.config")
+
 var corsOptions = {
-  origin: "http://localhost:8081",
-  origin: "http://localhost:3000"
+    origin: "http://localhost:8081",
+    origin: "http://localhost:3000"
 };
 
-var logger = function(req, res, next) {
-  console.log("GOT REQUEST !");
-  console.log(req)
-  next(); // Passing the request to the next handler in the stack.
-}
-
-app.use(logger);
-
 app.use(cors(corsOptions));
-
 // parse requests of content-type - application/json
-// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
 
-// database
-const db = require("./models");
-const Role = db.role;
-
-
-//db.sequelize.sync();
-// force: true will drop the table if it already exists
-db.sequelize.sync({force: true}).then(() => {
-  console.log('Drop and Resync Database with { force: true }');
-  initial();
+var con = mysql.createConnection({
+    host: config.HOST,
+    user: config.USER,
+    password: config.PASSWORD
 });
 
+app.get("/api/test/all", (req,res) => {
+    res.status(200).send("Custom content from backend");
+})
 
-
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected to SQL server!");
 });
 
-require('./routes/auth.routes')(app);
-require('./routes/user.routes')(app);
-
-// set port, listen for requests
-// THE EVN PROCESS DOES NOT WORK?
 const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
-
-
-function initial() {
-    Role.create({
-      id: 1,
-      name: "user"
-    });
-   
-    Role.create({
-      id: 2,
-      name: "moderator"
-    });
-   
-    Role.create({
-      id: 3,
-      name: "admin"
-    });
-  }
